28f735bc9199e4cdbbc4b9bc2e3caca2d45e01b9
Zach Harmon <zachha@gmail.com>
2019-03-07 01:17:57 -0500

    Add jsAlgorithm.js file with completed chessboard alg

---
+++ Responsive Web Design Projects/jsAlgorithms.js
@@ -0,0 +1,28 @@
+
+function chessBoard(len, wid) {
+  	// declare variables for starting row and odd/even rows
+	let rowStart = " #";
+	let rowOdd = "\n# ";
+  	let rowEven = "\n #";
+  	let boardString = "";
+  
+  	// loop through the defined width to build the columns out
+ 	for (let i = 0; i < (wid - 1); i++) {
+    	rowStart += " #";
+      	rowOdd += "# ";
+        rowEven += " #";
+        //console.log(rowStart);
+    }
+  
+  	// loop through the defined length to build the rows out
+  	for (let j = 0; j < len; j++) {
+    	if (!j) {
+          boardString = rowStart;
+        } else if (!(j % 2)) {
+          boardString += rowEven;
+        } else {
+          boardString += rowOdd;
+        }
+  	}
+  console.log(boardString);
+}
